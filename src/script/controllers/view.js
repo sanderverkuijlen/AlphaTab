197572a1149216a0d89c58ade7b3bf7c5e960ad1
Sander Verkuijlen <sanderverkuijlen@gmail.com>
2014-07-31 22:28:53 +0200

    Trial stuff

--- src/script/controllers/view.js
+++ src/script/controllers/view.js
@@ -15,33 +15,33 @@ app.controller('ViewCtrl', ['$scope', '$filter', function($scope, $filter){
     };
 
     //Column-layout
-    $scope.layouts = {
-        current_id:     1,
-        current:        function(){
-            return $filter('filter')($scope.layouts.all, {id: $scope.layouts.current_id})[0];
-        },
-        all:            [
-            {   id:         1,
-                name:       'Default',
-                template:   'default',
-                class:      'default',
-                lists:      [
-                    {   id:         1,
-                        widgets:    [4,5]
-                    },
-                    {   id:         2,
-                        widgets:    [1,2,3]
-                    },
-                    {   id:         3,
-                        widgets:    [6]
-                    },
-                    {   id:         4,
-                        widgets:    [7]
-                    },
-                    {   id:         5,
-                        widgets:    [8,9]
-                    }
-                ]
-            }
-    ]};
+//    $scope.layouts = {
+//        current_id:     1,
+//        current:        function(){
+//            return $filter('filter')($scope.layouts.all, {id: $scope.layouts.current_id})[0];
+//        },
+//        all:            [
+//            {   id:         1,
+//                name:       'Default',
+//                template:   'default',
+//                class:      'default',
+//                lists:      [
+//                    {   id:         1,
+//                        widgets:    [4,5]
+//                    },
+//                    {   id:         2,
+//                        widgets:    [1,2,3]
+//                    },
+//                    {   id:         3,
+//                        widgets:    [6]
+//                    },
+//                    {   id:         4,
+//                        widgets:    [7]
+//                    },
+//                    {   id:         5,
+//                        widgets:    [8,9]
+//                    }
+//                ]
+//            }
+//    ]};
 }]);
